18f665e319eaf4b23490cd37e0df899c75b3f732
Navaneethan G <smashernavaneethan12@gmail.com>
2022-03-02 13:07:06 +0530

    first commit

---
+++ FirebaseLetsBuildTheApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  FirebaseLetsBuildTheApp
+//
+//  Created by navaneeth-pt4855 on 02/03/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
---
+++ FirebaseLetsBuildTheApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseLetsBuildTheApp
+//
+//  Created by navaneeth-pt4855 on 02/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
